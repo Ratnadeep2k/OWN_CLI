f3492456097179e0455b66ea975995eceb47e23e
ratnadeep2k <ratnadeepboruah365@gmail.com>
2024-03-28 13:34:06 +0530

    Module

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
+import { count } from './utils.js'
 const note =process.argv[2];
 const newNote ={
     content:note,
     id:Date.now()
 }
 console.log(newNote);
+
+//isolate code
+//it canot be accessed from outside
+// (function(){
+//     console.log('IIFE');
+// }) ();
 
 //CLI command
 //Chatting with the terminal
